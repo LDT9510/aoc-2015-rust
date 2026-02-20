5ac49d82bccbf3e0bda5ae867516f711077b3306
LDT9510 <luis.ernesto951008@gmail.com>
2026-02-20 16:40:31 -0300

    Finish day 15

---
+++ src/bin/15.rs
@@ -0,0 +1,99 @@
+use advent_of_code::utils::parsing::IterInts;
+use itertools::Itertools;
+
+advent_of_code::solution!(15);
+
+struct Ingredient {
+    capacity: i64,
+    durability: i64,
+    flavor: i64,
+    texture: i64,
+    calories: i64,
+}
+
+fn parse_ingredients(input: &str) -> Vec<Ingredient> {
+    let mut ingredients = Vec::new();
+
+    for line in input.lines() {
+        if let Some((capacity, durability, flavor, texture, calories)) =
+            line.iter_ints().collect_tuple()
+        {
+            ingredients.push(Ingredient {
+                capacity,
+                durability,
+                flavor,
+                texture,
+                calories,
+            })
+        }
+    }
+
+    ingredients
+}
+
+fn calculate_total_score(
+    ingredients: &[Ingredient],
+    teaspoons: &[i64],
+    target_calories: Option<i64>,
+) -> i64 {
+    let (capacity_score, durability_score, texture_score, flavor_score, total_calories) =
+        ingredients
+            .iter()
+            .zip(teaspoons)
+            .fold((0, 0, 0, 0, 0), |(c, d, t, f, ca), (ig, &ts)| {
+                (
+                    c + ig.capacity * ts,
+                    d + ig.durability * ts,
+                    t + ig.texture * ts,
+                    f + ig.flavor * ts,
+                    ca + ig.calories * ts,
+                )
+            });
+
+    if let Some(calories) = target_calories
+        && total_calories != calories
+    {
+        return 0;
+    }
+
+    capacity_score.max(0) * durability_score.max(0) * texture_score.max(0) * flavor_score.max(0)
+}
+
+fn solve(ingredients: &[Ingredient], target_calories: Option<i64>) -> Option<i64> {
+    const TOTAL_INGREDIENTS: i64 = 100;
+
+    (0..ingredients.len())
+        .map(|_| 0..TOTAL_INGREDIENTS)
+        .multi_cartesian_product()
+        .filter(|teaspoons| teaspoons.iter().sum::<i64>() == TOTAL_INGREDIENTS)
+        .map(|teaspoons| calculate_total_score(ingredients, &teaspoons, target_calories))
+        .max()
+}
+
+pub fn part_one(input: &str) -> Option<i64> {
+    let ingredients = parse_ingredients(input);
+
+    solve(&ingredients, None)
+}
+pub fn part_two(input: &str) -> Option<i64> {
+    let ingredients = parse_ingredients(input);
+
+    solve(&ingredients, Some(500))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(62_842_880));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(57_600_000));
+    }
+}
--- src/utils/parsing.rs
+++ src/utils/parsing.rs
@@ -1,3 +1,4 @@
+use std::marker::PhantomData;
 use std::str::FromStr;
 
 pub trait UnwrapNextInt {
@@ -12,5 +13,88 @@ impl<'a, U: Iterator<Item = &'a str>> UnwrapNextInt for U {
         <T as FromStr>::Err: std::fmt::Debug,
     {
         self.next().unwrap().parse::<T>().unwrap()
+    }
+}
+
+pub trait IterInts {
+    fn iter_ints<T: FromStr>(&self) -> IntParser<'_, T>;
+}
+
+impl IterInts for &str {
+    fn iter_ints<T: FromStr>(&self) -> IntParser<'_, T> {
+        IntParser {
+            slice: self,
+            _phantom_data: PhantomData,
+        }
+    }
+}
+
+pub struct IntParser<'a, T> {
+    slice: &'a str,
+    _phantom_data: PhantomData<T>,
+}
+
+impl<T: FromStr> Iterator for IntParser<'_, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        fn part_of_digit(byte: u8, curr_idx: usize, slice: &[u8]) -> bool {
+            let is_negative_sign =
+                curr_idx < slice.len() && (byte == b'-' && slice[curr_idx + 1].is_ascii_digit());
+
+            is_negative_sign || byte.is_ascii_digit()
+        }
+
+        let bytes = self.slice.as_bytes();
+        let mut start_idx = 0;
+        let mut end_idx = bytes.len();
+        let mut last_char_was_digit = false;
+
+        for i in 0..bytes.len() {
+            let b = bytes[i];
+            let is_part_of_digit = part_of_digit(b, i, bytes);
+
+            if is_part_of_digit && !last_char_was_digit {
+                start_idx = i;
+                last_char_was_digit = true;
+            } else if !is_part_of_digit && last_char_was_digit {
+                end_idx = i;
+                break;
+            }
+        }
+
+        let parsing_result = self.slice[start_idx..end_idx].parse::<T>().ok();
+        self.slice = &self.slice[end_idx..];
+        parsing_result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unwrap_next_int() {
+        let text = "a -1111 b 2222 3";
+        let mut split = text.split_ascii_whitespace();
+        // skip 'a'
+        split.next();
+        assert_eq!(split.unwrap_next_int::<i64>(), -1111);
+        // skip 'b'
+        split.next();
+        assert_eq!(split.unwrap_next_int::<u64>(), 2222);
+        assert_eq!(split.unwrap_next_int::<usize>(), 3);
+    }
+
+    #[test]
+    fn test_iter_ints() {
+        let mut v: Vec<i64> = "aaaa 123 bbbb3434 cccc -1123".iter_ints().collect();
+        assert_eq!(v, vec![123, 3434, -1123]);
+
+        v = "43432234".iter_ints().collect();
+        assert_eq!(v, vec![43432234]);
+
+        v = "aaaa 123 b-333 8".iter_ints().collect();
+        assert_eq!(v, vec![123, -333, 8]);
     }
 }
